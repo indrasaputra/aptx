ae485c55cbfcc6121092bacaa0cca9b720d29da8
Indra Saputra <flame.fire99@gmail.com>
2021-03-19 16:30:14 +0700

    test when url is empty

--- usecase/short_url_creator_test.go
+++ usecase/short_url_creator_test.go
@@ -1,11 +1,13 @@
 package usecase_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/indrasaputra/shortener/entity"
 	mock_usecase "github.com/indrasaputra/shortener/test/mock/usecase"
 	"github.com/indrasaputra/shortener/usecase"
 )
@@ -23,6 +25,24 @@ func TestNewShortURLCreator(t *testing.T) {
 	t.Run("successfully create an instance of ShortURLCreator", func(t *testing.T) {
 		exec := createShortURLCreatorExecutor(ctrl)
 		assert.NotNil(t, exec.usecase)
+	})
+}
+
+func TestShortURLCreator_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("empty url can't be processedd", func(t *testing.T) {
+		exec := createShortURLCreatorExecutor(ctrl)
+		urls := []string{"", "   ", "        ", "     "}
+
+		for _, url := range urls {
+			res, err := exec.usecase.Create(context.Background(), url)
+
+			assert.NotNil(t, err)
+			assert.Equal(t, entity.ErrEmptyURL, err)
+			assert.Nil(t, res)
+		}
 	})
 }
 
