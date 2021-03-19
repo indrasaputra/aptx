580b9397a84c0bbde5aa1cc4edd45f55f5958dce
Indra Saputra <flame.fire99@gmail.com>
2021-03-19 10:55:49 +0700

    create bash to generate protobuf

---
+++ bin/generate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+IMAGE_DIR=image
+
+buf lint
+buf breaking --against '.git#branch=main'
+buf generate
+
+mkdir -p ${IMAGE_DIR}
+buf build -o ${IMAGE_DIR}/image.bin
