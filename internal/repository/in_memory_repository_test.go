cbe5753f5b0eaf7e74f326118ca5a3293ac5e330
Indra Saputra <flame.fire99@gmail.com>
2021-03-22 14:13:57 +0700

    test in-memory url repository

--- internal/repository/in_memory_repository_test.go
+++ internal/repository/in_memory_repository_test.go
@@ -1,1 +1,57 @@
 package repository_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/shortener/entity"
+	"github.com/indrasaputra/shortener/internal/repository"
+)
+
+func TestNewInMemoryURLRepository(t *testing.T) {
+	repo := repository.NewInMemoryURLRepository()
+	assert.NotNil(t, repo)
+}
+
+func TestInMemoryURLRepository_Save(t *testing.T) {
+	t.Run("can't save duplicated short URL", func(t *testing.T) {
+		shorts := []string{"http://short-1.url", "http://short-2.url", "http://short-3.url"}
+
+		repo := repository.NewInMemoryURLRepository()
+		for i, short := range shorts {
+			url := createURL(fmt.Sprintf("http://original-%d.url", i), short)
+			repo.Save(context.Background(), url)
+		}
+
+		for i, short := range shorts {
+			url := createURL(fmt.Sprintf("http://original-%d-%d.url", i, i), short)
+			err := repo.Save(context.Background(), url)
+
+			assert.NotNil(t, err)
+			assert.Equal(t, entity.ErrDuplicatedShortURL, err)
+		}
+	})
+
+	t.Run("success save short url", func(t *testing.T) {
+		shorts := []string{"http://short-1.url", "http://short-2.url", "http://short-3.url"}
+
+		repo := repository.NewInMemoryURLRepository()
+		for i, short := range shorts {
+			url := createURL(fmt.Sprintf("http://original-%d.url", i), short)
+			err := repo.Save(context.Background(), url)
+			assert.Nil(t, err)
+		}
+	})
+}
+
+func createURL(original, short string) *entity.URL {
+	return &entity.URL{
+		ShortURL:    short,
+		OriginalURL: original,
+		ExpiredAt:   time.Now().Add(1 * time.Hour),
+	}
+}
