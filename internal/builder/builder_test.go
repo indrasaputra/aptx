22a42e6c2fa54f850103e36569e020aedab8caa3
Indra Saputra <flame.fire99@gmail.com>
2021-03-24 10:03:04 +0700

    test BuildGRPCShortURLCreator

--- internal/builder/builder_test.go
+++ internal/builder/builder_test.go
@@ -1,1 +1,16 @@
 package builder_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/url-shortener/internal/builder"
+)
+
+func TestBuildGRPCShortURLCreator(t *testing.T) {
+	t.Run("successfully create ShortURLCreator handler", func(t *testing.T) {
+		hdr := builder.BuildGRPCShortURLCreator("http://short-url.com")
+		assert.NotNil(t, hdr)
+	})
+}
