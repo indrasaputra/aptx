06252e7bfa7b1155bc657731abf1b6d302f6571c
Indra Saputra <flame.fire99@gmail.com>
2021-03-24 09:11:27 +0700

    test when nil request is sent

--- internal/http2/grpc/handler/url_shortener_test.go
+++ internal/http2/grpc/handler/url_shortener_test.go
@@ -1,11 +1,13 @@
 package handler_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/indrasaputra/url-shortener/entity"
 	"github.com/indrasaputra/url-shortener/internal/http2/grpc/handler"
 	mock_usecase "github.com/indrasaputra/url-shortener/test/mock/usecase"
 )
@@ -22,6 +24,21 @@ func TestNewShortURLCreator(t *testing.T) {
 	t.Run("successful create an instance of ShortURLCreator", func(t *testing.T) {
 		exec := createShortURLCreatorExecutor(ctrl)
 		assert.NotNil(t, exec.handler)
+	})
+}
+
+func TestShortURLCreator_CreateShortURL(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("empty url is prohibited", func(t *testing.T) {
+		exec := createShortURLCreatorExecutor(ctrl)
+
+		resp, err := exec.handler.CreateShortURL(context.Background(), nil)
+
+		assert.NotNil(t, exec.handler)
+		assert.NotNil(t, entity.ErrEmptyURL, err)
+		assert.Nil(t, resp)
 	})
 }
 
