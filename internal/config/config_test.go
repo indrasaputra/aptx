0c0cdf0abde03f96f31b581a1b004ad9023c2022
Indra Saputra <flame.fire99@gmail.com>
2021-03-24 10:08:41 +0700

    test config

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,1 +1,25 @@
 package config_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/url-shortener/internal/config"
+)
+
+func TestNewConfig(t *testing.T) {
+	t.Run("fail to create an instance of Config due to incomplete env", func(t *testing.T) {
+		cfg, err := config.NewConfig("test/fixture/env.incomplete")
+
+		assert.NotNil(t, err)
+		assert.Nil(t, cfg)
+	})
+
+	t.Run("successfully create an instance of Config", func(t *testing.T) {
+		cfg, err := config.NewConfig("../../test/fixture/env.valid")
+
+		assert.Nil(t, err)
+		assert.NotNil(t, cfg)
+	})
+}
