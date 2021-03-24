d6bb152882fff81bcfb83a175d9517040657f6ea
Indra Saputra <flame.fire99@gmail.com>
2021-03-24 08:44:52 +0700

    test constructor of ShortURLCreator

--- internal/http2/grpc/handler/url_shortener_test.go
+++ internal/http2/grpc/handler/url_shortener_test.go
@@ -1,1 +1,35 @@
 package handler_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/shortener/internal/http2/grpc/handler"
+	mock_usecase "github.com/indrasaputra/shortener/test/mock/usecase"
+)
+
+type ShortURLCreatorExecutor struct {
+	handler *handler.ShortURLCreator
+	usecase *mock_usecase.MockCreateShortURL
+}
+
+func TestNewShortURLCreator(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("successful create an instance of ShortURLCreator", func(t *testing.T) {
+		exec := createShortURLCreatorExecutor(ctrl)
+		assert.NotNil(t, exec.handler)
+	})
+}
+
+func createShortURLCreatorExecutor(ctrl *gomock.Controller) *ShortURLCreatorExecutor {
+	c := mock_usecase.NewMockCreateShortURL(ctrl)
+	h := handler.NewShortURLCreator(c)
+	return &ShortURLCreatorExecutor{
+		handler: h,
+		usecase: c,
+	}
+}
