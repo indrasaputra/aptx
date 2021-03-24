f985b6252c78c5690547e1e23609cef5bb9592ac
Indra Saputra <flame.fire99@gmail.com>
2021-03-24 10:22:16 +0700

    implement server main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/indrasaputra/url-shortener/internal/builder"
+	"github.com/indrasaputra/url-shortener/internal/config"
+	shortenerv1 "github.com/indrasaputra/url-shortener/proto/indrasaputra/shortener/v1"
+)
+
+func main() {
+	cfg, err := config.NewConfig(".env")
+	checkError(err)
+
+	server := grpc.NewServer()
+
+	urlCreator := builder.BuildGRPCShortURLCreator(cfg.Domain)
+	shortenerv1.RegisterURLShortenerServiceServer(server, urlCreator)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.Port))
+	checkError(err)
+
+	server.Serve(lis)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
