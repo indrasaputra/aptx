cb47ad8d7546e9717198b95102bffbb55512a634
Indra Saputra <flame.fire99@gmail.com>
2021-03-28 06:56:24 +0700

    implement get single url

--- usecase/url_getter.go
+++ usecase/url_getter.go
@@ -10,12 +10,17 @@ import (
 type GetURL interface {
 	// GetAll gets all URL available in system.
 	GetAll(ctx context.Context) ([]*entity.URL, *entity.Error)
+	// GetByShortURL gets a single data available in system based on short URL.
+	GetByShortURL(ctx context.Context, shortURL string) (*entity.URL, *entity.Error)
 }
 
 // GetURLRepository defines the contract to get URL.
 type GetURLRepository interface {
 	// GetAll gets all URL available in repository.
 	GetAll(ctx context.Context) ([]*entity.URL, *entity.Error)
+	// GetByShortURL gets a single data available in repository based on short URL.
+	// If the data is not found, it returns ErrURLNotFound.
+	GetByShortURL(ctx context.Context, shortURL string) (*entity.URL, *entity.Error)
 }
 
 // URLGetter is responsible to get URL.
@@ -31,4 +36,9 @@ func NewURLGetter(repo GetURLRepository) *URLGetter {
 // GetAll gets all URLs in the system.
 func (ug *URLGetter) GetAll(ctx context.Context) ([]*entity.URL, *entity.Error) {
 	return ug.repo.GetAll(ctx)
+}
+
+// GetByShortURL gets a single data available in system based on short URL.
+func (ug *URLGetter) GetByShortURL(ctx context.Context, shortURL string) (*entity.URL, *entity.Error) {
+	return ug.repo.GetByShortURL(ctx, shortURL)
 }
