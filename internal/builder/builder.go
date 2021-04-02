6a6409a6d58bfd9f25778b7fe8ba180e0e692ba2
Indra Saputra <flame.fire99@gmail.com>
2021-04-03 06:33:40 +0700

    create health checker builder

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -7,11 +7,18 @@ import (
 	"github.com/indrasaputra/url-shortener/usecase"
 )
 
-// BuildGRPCURLShortener build URLShortener handler together with all of its dependencies.
+// BuildGRPCURLShortener builds URLShortener handler together with all of its dependencies.
 func BuildGRPCURLShortener(domain string) *handler.URLShortener {
 	gen := tool.NewShortURLGenerator(domain)
 	repo := repository.NewInMemoryURLRepository()
 	creator := usecase.NewShortURLCreator(gen, repo)
 	getter := usecase.NewURLGetter(repo)
 	return handler.NewURLShortener(creator, getter)
+}
+
+// BuildGRPCHealthChecker builds HealthChecker handler together with all of its dependencies.
+func BuildGRPCHealthChecker() *handler.HealthChecker {
+	repo := repository.NewInMemoryURLRepository()
+	checker := usecase.NewHealthChecker(repo)
+	return handler.NewHealthChecker(checker)
 }
