4e1366a9845c84fea9fc49260eb4e95c2ce02cf1
Indra Saputra <flame.fire99@gmail.com>
2021-04-02 15:26:47 +0700

    create proto error

--- entity/error.go
+++ entity/error.go
@@ -1,6 +1,14 @@
 package entity
 
-import "fmt"
+import (
+	"fmt"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	shortenerv1 "github.com/indrasaputra/url-shortener/proto/indrasaputra/shortener/v1"
+)
 
 var (
 	// ErrInternalServer indicates there is unexpected problem occurs in the system itself.
@@ -50,5 +58,72 @@ func WrapError(err *Error, message string) *Error {
 		Code:            err.Code,
 		Message:         err.Message,
 		internalMessage: fmt.Sprintf("%s. %s", err.internalMessage, message),
+	}
+}
+
+// ErrInternal returns codes.Internal.
+// It explains that unexpected behavior occurred in system.
+func ErrInternal(message string) error {
+	st := status.New(codes.Internal, message)
+	se := &shortenerv1.URLShortenerError{
+		ErrorCode: shortenerv1.URLShortenerErrorCode_INTERNAL,
+	}
+	res, err := st.WithDetails(se)
+	if err != nil {
+		return st.Err()
+	}
+	return res.Err()
+}
+
+// ErrNilURL returns codes.InvalidArgument.
+// It explains that the instance is empty or nil.
+func ErrNilURL() error {
+	st := status.New(codes.InvalidArgument, "")
+	br := createBadRequest(&errdetails.BadRequest_FieldViolation{
+		Field:       "URL instance",
+		Description: "empty or nil",
+	})
+
+	se := &shortenerv1.URLShortenerError{
+		ErrorCode: shortenerv1.URLShortenerErrorCode_EMPTY_URL,
+	}
+	res, err := st.WithDetails(br, se)
+	if err != nil {
+		return st.Err()
+	}
+	return res.Err()
+}
+
+// ErrAlreadyExists returns codes.AlreadyExists.
+// It explains that the code / short URL already exists.
+func ErrAlreadyExists() error {
+	st := status.New(codes.AlreadyExists, "")
+	se := &shortenerv1.URLShortenerError{
+		ErrorCode: shortenerv1.URLShortenerErrorCode_ALREADY_EXISTS,
+	}
+	res, err := st.WithDetails(se)
+	if err != nil {
+		return st.Err()
+	}
+	return res.Err()
+}
+
+// ErrNotFound returns codes.NotFound.
+// It explains that short URL is not found.
+func ErrNotFound(message string) error {
+	st := status.New(codes.NotFound, message)
+	se := &shortenerv1.URLShortenerError{
+		ErrorCode: shortenerv1.URLShortenerErrorCode_NOT_FOUND,
+	}
+	res, err := st.WithDetails(se)
+	if err != nil {
+		return st.Err()
+	}
+	return res.Err()
+}
+
+func createBadRequest(details ...*errdetails.BadRequest_FieldViolation) *errdetails.BadRequest {
+	return &errdetails.BadRequest{
+		FieldViolations: details,
 	}
 }
