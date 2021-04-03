32a5982753841252ad79e176d6a640e1aadc854a
Indra Saputra <flame.fire99@gmail.com>
2021-04-03 15:11:23 +0700

    add postgres to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,11 +6,23 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Postgres holds configuration for PostgreSQL.
+type Postgres struct {
+	Host         string `env:"POSTGRES_HOST,required"`
+	Port         string `env:"POSTGRES_PORT,required"`
+	DBName       string `env:"POSTGRES_DBNAME,required"`
+	User         string `env:"POSTGRES_USER,required"`
+	Password     string `env:"POSTGRES_PASSWORD,required"`
+	MaxOpenConns int    `env:"POSTGRES_MAX_OPEN_CONNS,required"`
+	MaxIdleConns int    `env:"POSTGRES_MAX_IDLE_CONNS,required"`
+}
+
 // Config holds configuration for the project.
 type Config struct {
 	PortHTTP string `env:"PORT_HTTP,default=8081"`
 	PortGRPC string `env:"PORT_GRPC,default=8080"`
 	Domain   string `env:"DOMAIN,required"`
+	Postgres Postgres
 }
 
 // NewConfig creates an instance of Config.
