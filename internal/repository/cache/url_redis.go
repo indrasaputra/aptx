89e8dd6840556ca1906eab1bba08aec9191a18a8
Indra Saputra <flame.fire99@gmail.com>
2021-04-04 18:58:28 +0700

    implement redis as cache

---
+++ internal/repository/cache/url_redis.go
@@ -0,0 +1,91 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/indrasaputra/url-shortener/entity"
+)
+
+var (
+	attributes        = []string{"code", "short_url", "original_url", "_expired_at", "created_at"}
+	numberOfAttribute = len(attributes)
+)
+
+// URLRedis is responsible to connect with Redis.
+// It uses https://github.com/go-redis/redis.
+type URLRedis struct {
+	client redis.Cmdable
+}
+
+// NewURLRedis creates an instance URLRedis.
+func NewURLRedis(client redis.Cmdable) *URLRedis {
+	return &URLRedis{client: client}
+}
+
+// Save saves URL in Redis.
+// It uses hash (https://redis.io/commands/hset).
+func (ur *URLRedis) Save(ctx context.Context, url *entity.URL) error {
+	hash := createURLHash(url)
+
+	pipe := ur.client.Pipeline()
+	res := pipe.HSet(ctx, url.Code, hash)
+	pipe.ExpireAt(ctx, url.Code, url.ExpiredAt)
+	_, err := pipe.Exec(ctx)
+
+	if int(res.Val()) != numberOfAttribute {
+		return entity.ErrInternal(fmt.Sprintf("only success to save %d out of %d attributes", res.Val(), numberOfAttribute))
+	}
+	if err != nil {
+		return entity.ErrInternal(err.Error())
+	}
+	return nil
+}
+
+// Get gets URL detail from Redis.
+func (ur *URLRedis) Get(ctx context.Context, key string) (*entity.URL, error) {
+	hash, err := ur.client.HGetAll(ctx, key).Result()
+	if err != nil {
+		return nil, entity.ErrInternal(err.Error())
+	}
+	if len(hash) == 0 {
+		return nil, entity.ErrNotFound()
+	}
+	return createURLFromHash(hash)
+}
+
+func createURLFromHash(hash map[string]string) (*entity.URL, error) {
+	url := &entity.URL{}
+	var err error
+
+	url.Code = hash["code"]
+	url.ShortURL = hash["short_url"]
+	url.OriginalURL = hash["original_url"]
+	url.ExpiredAt, err = time.Parse(time.RFC3339, hash["expired_at"])
+	if err != nil {
+		return nil, entity.ErrInternal(err.Error())
+	}
+	url.CreatedAt, err = time.Parse(time.RFC3339, hash["created_at"])
+	if err != nil {
+		return nil, entity.ErrInternal(err.Error())
+	}
+	return url, nil
+}
+
+func createURLHash(url *entity.URL) []string {
+	return []string{
+		"code",
+		url.Code,
+		"short_url",
+		url.ShortURL,
+		"original_url",
+		url.OriginalURL,
+		"expired_at",
+		url.ExpiredAt.Format(time.RFC3339),
+		"created_at",
+		url.CreatedAt.Format(time.RFC3339),
+	}
+}
---
+++ internal/repository/cache/url_redis_test.go
@@ -0,0 +1,170 @@
+package cache_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redismock/v8"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/indrasaputra/url-shortener/entity"
+	"github.com/indrasaputra/url-shortener/internal/repository/cache"
+)
+
+var (
+	testURLCode      = "AbCdE12"
+	testURLShort     = "http://localhost/" + testURLCode
+	testURLOriginal  = "http://very-long-url.url"
+	testURLExpiredAt = time.Now().Add(1 * time.Minute)
+	testURLCreatedAt = time.Now()
+	testURL          = &entity.URL{
+		Code:        testURLCode,
+		ShortURL:    testURLShort,
+		OriginalURL: testURLOriginal,
+		ExpiredAt:   testURLExpiredAt,
+		CreatedAt:   testURLCreatedAt,
+	}
+	testEmptyMapResult = make(map[string]string)
+	testValidMapResult = map[string]string{
+		"code":         testURLCode,
+		"short_url":    testURLShort,
+		"original_url": testURLOriginal,
+		"expired_at":   testURLExpiredAt.Format(time.RFC3339),
+		"created_at":   testURLCreatedAt.Format(time.RFC3339),
+	}
+	testHSetInput = []string{
+		"code",
+		testURLCode,
+		"short_url",
+		testURLShort,
+		"original_url",
+		testURLOriginal,
+		"expired_at",
+		testURLExpiredAt.Format(time.RFC3339),
+		"created_at",
+		testURLCreatedAt.Format(time.RFC3339),
+	}
+)
+
+type URLRedisExecutor struct {
+	redis *cache.URLRedis
+	mock  redismock.ClientMock
+}
+
+func TestNewURLRedis(t *testing.T) {
+	t.Run("successfully create an instance of Redis", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		assert.NotNil(t, exec.redis)
+	})
+}
+
+func TestURLRedis_Save(t *testing.T) {
+	t.Run("not all attributes are saved", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		exec.mock.ExpectHSet(testURLCode, testHSetInput).SetVal(2)
+		exec.mock.ExpectExpireAt(testURLCode, testURLExpiredAt).SetVal(true)
+
+		err := exec.redis.Save(context.Background(), testURL)
+
+		assert.NotNil(t, err)
+		assert.Contains(t, err.Error(), "only success to save 2 out of 5 attributes")
+	})
+
+	t.Run("redis is down", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		exec.mock.ExpectHSet(testURLCode, testHSetInput).SetVal(5)
+		exec.mock.ExpectExpireAt(testURLCode, testURLExpiredAt).SetErr(errors.New("redis down"))
+
+		err := exec.redis.Save(context.Background(), testURL)
+
+		assert.NotNil(t, err)
+		assert.Contains(t, err.Error(), "redis down")
+	})
+
+	t.Run("success save url in redis hash", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		exec.mock.ExpectHSet(testURLCode, testHSetInput).SetVal(5)
+		exec.mock.ExpectExpireAt(testURLCode, testURLExpiredAt).SetVal(true)
+
+		err := exec.redis.Save(context.Background(), testURL)
+
+		assert.Nil(t, err)
+	})
+}
+
+func TestURLRedis_Get(t *testing.T) {
+	t.Run("redis hgetall returns error", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		exec.mock.ExpectHGetAll(testURLCode).SetErr(errors.New("redis down"))
+
+		url, err := exec.redis.Get(context.Background(), testURLCode)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, codes.Internal, status.Code(err))
+		assert.Nil(t, url)
+	})
+
+	t.Run("redis hgetall returns empty hash", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		exec.mock.ExpectHGetAll(testURLCode).SetVal(testEmptyMapResult)
+
+		url, err := exec.redis.Get(context.Background(), testURLCode)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, codes.NotFound, status.Code(err))
+		assert.Nil(t, url)
+	})
+
+	t.Run("url's expired_at is invalid", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		hash := make(map[string]string)
+		hash["expired_at"] = ""
+		exec.mock.ExpectHGetAll(testURLCode).SetVal(hash)
+
+		url, err := exec.redis.Get(context.Background(), testURLCode)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, codes.Internal, status.Code(err))
+		assert.Nil(t, url)
+	})
+
+	t.Run("url's created_at is invalid", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		hash := make(map[string]string)
+		hash["expired_at"] = "2021-04-04T12:05:38.728727+07:00"
+		hash["created_at"] = ""
+		exec.mock.ExpectHGetAll(testURLCode).SetVal(hash)
+
+		url, err := exec.redis.Get(context.Background(), testURLCode)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, codes.Internal, status.Code(err))
+		assert.Nil(t, url)
+	})
+
+	t.Run("success get url from redis hash", func(t *testing.T) {
+		exec := createURLRedisExecutor()
+		exec.mock.ExpectHGetAll(testURLCode).SetVal(testValidMapResult)
+
+		url, err := exec.redis.Get(context.Background(), testURLCode)
+		fmt.Println(err)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, url)
+	})
+}
+
+func createURLRedisExecutor() *URLRedisExecutor {
+	client, mock := redismock.NewClientMock()
+	rds := cache.NewURLRedis(client)
+	// mock.Exc
+	return &URLRedisExecutor{
+		redis: rds,
+		mock:  mock,
+	}
+}
